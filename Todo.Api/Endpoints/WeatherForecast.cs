78bcbd307ae6784f5c57e5777e71120f4cea2ca7
Robert E. Cato <robertcato015@gmail.com>
2025-02-01 17:44:00 -0600

    Endpoint mapping

---
+++ Todo.Api/Endpoints/WeatherForecast.cs
@@ -0,0 +1,31 @@
+﻿using Todo.Api.Entities;
+
+namespace Todo.Api.Endpoints;
+
+public static class WeatherForecastEndpoints
+{
+    public static IEndpointRouteBuilder MapWeatherForecastEndpoints(this WebApplication app)
+    {
+        var weather = app.MapGroup("/weatherforecast");
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        weather.MapGet("/", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        }).WithName("GetWeatherForecast");
+
+        return weather;
+    }
+}
---
+++ Todo.Api/Middleware/WebApplicationEndpointMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using Todo.Api.Endpoints;
+
+namespace Todo.Api.Middleware;
+
+public static class WebApplicationEndpointMiddleware
+{
+    public static WebApplication MapApiEndpoints(this WebApplication app)
+    {
+        app.MapWeatherForecastEndpoints();
+        return app;
+    }
+}
