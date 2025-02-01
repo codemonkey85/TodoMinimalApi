9f5897d9836c5c04c4e39c4139b2c8364f5bc675
Robert E. Cato <robertcato015@gmail.com>
2025-02-01 17:53:06 -0600

    Serilog

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -1,21 +1,39 @@
+using Serilog;
 using Todo.Api.Middleware;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    IServiceCollection? services = builder.Services;
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    services.AddOpenApi();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.MapApiEndpoints();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.MapApiEndpoints();
-
-app.Run();
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "{exMessage}", ex.Message);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
