e621ded54f1e3ec2cc274b58da9adc6a2a686f44
Robert E. Cato <rcato@ci.thibodaux.la.us>
2025-02-12 08:32:07 -0600

    React: Pre-app setup

--- Todo.React/vite.config.ts
+++ Todo.React/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { fileURLToPath } from "url";
@@ -6,29 +6,34 @@ import { fileURLToPath } from "url";
 const baseFolder =
     process.env.APPDATA !== undefined && process.env.APPDATA !== ''
         ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
-
+    : `${process.env.HOME}/.aspnet/https`;
+        
 // https://vite.dev/config/
-export default defineConfig({
-  //  define: { API_URL: `"${apiUrl}"` },
-  plugins: [react(), basicSsl(
-    {
-      name: 'todo-react',
-      certDir: baseFolder
-    }
-  )],
-  resolve: {
-    alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  },
-  server: {
-    proxy: {
-        '^/api': {
-            target: "https://localhost:7128/api",
-            secure: true
-        }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define:
+      { __API_URL__: JSON.stringify(env.VITE_API_URL) },
+    plugins: [react(), basicSsl(
+      {
+        name: 'todo-react',
+        certDir: baseFolder
+      }
+    )],
+    resolve: {
+      alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }
     },
-    port: 5173,
+    server: {
+      proxy: {
+          '^/api': {
+              target: "https://localhost:7128/api",
+              secure: true
+          }
+      },
+      port: 5173,
+    }
   }
 })
