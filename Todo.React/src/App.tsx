5b3e87b56d070123967f12934ccfdb8cd236380b
Robert E. Cato <rcato@ci.thibodaux.la.us>
2025-02-13 09:18:58 -0600

    Weather Forecast page

--- Todo.React/src/App.tsx
+++ Todo.React/src/App.tsx
@@ -1,59 +1,7 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import { IWeatherForecast, WeatherForecastListSchema } from './models'
-import { up, isResponseError, isValidationError } from 'up-fetch'
 
-function App() {
-  const upfetch = up(fetch, () => ({
-    baseUrl: __API_URL__
-  }))
-  const [forecasts, setForecasts] = useState<IWeatherForecast[]>([])
-  
-  useEffect(() => {
-    const fetchForecasts = async () => {
-      try {
-        const data = await upfetch('/api/weatherforecast',
-          { schema: WeatherForecastListSchema }
-        )
-        setForecasts(data)
-        console.log(data)
-      } catch (error) {
-        if (isResponseError(error)) {
-          console.log(error.status)
-        }
-        if (isValidationError(error)) {
-          console.error(error.issues)
-        }
-        else {
-          console.error(error)
-        }
-      }
-    }
-
-    fetchForecasts()
-  }, [])
-  return (
-    <table>
-      <thead>
-        <tr>
-            <th>Date</th>
-            <th aria-label="Temperature in Celsius">Temp. (C)</th>
-            <th aria-label="Temperature in Farenheit">Temp. (F)</th>
-            <th>Summary</th>
-        </tr>
-      </thead>
-      <tbody>
-        {forecasts.map((forecast) => (
-          <tr key={forecast.id}>
-            <td>{new Date(forecast.date).toLocaleString()}</td>
-            <td>{forecast.temperatureC}</td>
-            <td>{forecast.temperatureF}</td>
-            <td>{forecast.summary}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+export default function App() {
+    return (
+        <h2>It works!</h2>
+    )
 }
-
-export default App
---
+++ Todo.React/src/WeatherForecast.tsx
@@ -0,0 +1,46 @@
+import { IWeatherForecast, WeatherForecastListSchema } from './models'
+import { up } from 'up-fetch'
+import { useQuery } from '@tanstack/react-query'
+
+function WeatherForecast() {
+  const upfetch = up(fetch, () => ({
+    baseUrl: __API_URL__
+  }))
+
+  const { isPending, error, data } = useQuery<IWeatherForecast[]>({
+    queryKey: ['weatherForecasts'],
+    queryFn: async () => {
+      const data = await upfetch('/api/weatherforecast', { schema: WeatherForecastListSchema })
+      return data
+    }
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+  
+  return (
+    <table>
+      <thead>
+        <tr>
+            <th>Date</th>
+            <th aria-label="Temperature in Celsius">Temp. (C)</th>
+            <th aria-label="Temperature in Farenheit">Temp. (F)</th>
+            <th>Summary</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((forecast) => (
+          <tr key={forecast.id}>
+            <td>{new Date(forecast.date).toLocaleString()}</td>
+            <td>{forecast.temperatureC}</td>
+            <td>{forecast.temperatureF}</td>
+            <td>{forecast.summary}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default WeatherForecast
